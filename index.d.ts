f51c16d57e7f8bc6cea104c8e9bde6b9e0718b25
Jonathan Holmes <pcf1235@aut.ac.nz>
2018-11-27 15:19:32 +1300

    Fix createElement + fix up everything else.

--- index.d.ts
+++ index.d.ts
@@ -5,43 +5,63 @@ export as namespace Roact;
 
 
 declare namespace Roact {
+	interface RenderableClass {
+		new (...args: Array<any>):
+		{
+			render(): Element;
+		}
+	}
+
+	interface RenderablePropsClass<P> {
+		new (props: P): {
+			render(): Element;
+			props: P;
+		}
+	}
+
     /** A Roact Element */
     interface Element {
         type: Symbol,
-        component: string | IExtendedComponent,
+        component: string | RenderableClass,
         props: any;
         source?: string;
-    }
-
-    interface IExtendedComponent {
-        render: (self: any) => Element;
     }
 
     abstract class IComponent {
         abstract render(): Element;
     }
 
-    interface ComponentInstanceHandle { }
+    interface ComponentInstanceHandle { 
+		_key?: string;
+		_parent?: Instance;
+		_element?: string | RenderableClass;
+	}
 
-    /**
+	type Children = Element[] | {[name: string]: Element};
 
-     * Creates a new Roact element representing the given component. Elements are lightweight descriptions about what a Roblox Instance should look like, like a blueprint!
+	/**
+	 * Create a new roact element representing a custom roact component
+	 * @param component The component type to create
+	 * @param props The properties of the component
+	 * @param children The children of this element
+	 */
+    function createElement<T extends Roact.Component<any, P>, P>(
+        component: RenderablePropsClass<P>,
+        props?: Partial<P>,
+        children?: Children
+	): Element;
 
-     * The `children` argument is shorthand for adding a `Roact.Children` key to props. It should be specified as a dictionary of names to elements.
-
-     * `component` can be a string, a function, or a table created by `Component.extend`
-
-     * Setting `T` of createElement will show the properties of that type
-
-     * @param component The Component
-     * @param props The properties of this element
-     * @param children The children of this element
-     */
-    function createElement<T>(
-        component: string | IExtendedComponent,
-        props?: T | { [name: string]: any },
-        children?: Element[] | { [name: string]: Element }
-    ): Element
+	/**
+	 * Create a new roact element representing a ROBLOX instance
+	 * @param instanceName The name of the instance
+	 * @param props The properties of the instance
+	 * @param children The children of the element
+	 */
+	function createElement<T extends Instance>(
+		instanceName: string,
+		props?: {[name: string]: any},
+		children?: Children
+	): Element;
 
     /**
      * Creates a Roblox Instance given a Roact `element`, and optionally a `parent` to put it in, and a `key` to use as the instance's Name.
@@ -55,86 +75,12 @@ declare namespace Roact {
      * 
      * `reconcile` can be used to change the props of a component instance created with `mount` and is useful for putting Roact content into non-Roact applications.
      */
-    function reconcile(handle: ComponentInstanceHandle, component: Element): ComponentInstanceHandle;
+    function reconcile<T>(handle: ComponentInstanceHandle, component: Element): ComponentInstanceHandle;
 
     /**
      * Destroys the given `ComponentInstanceHandle` and all of its descendents. Does not operate on a Roblox Instance -- this must be given a handle that was returned by `Roact.mount`
      */
     function unmount(handle: ComponentInstanceHandle): void;
-    /**
-     * This is the key that Roact uses internally to store the children that are attached to a Roact element.
-     * 
-     * If you're writing a new functional or stateful element that needs to be used like a primitive component, you can access `Roact.Children` in your props table
-     * 
-     *      // example usage
-     *      let children = self.props[Roact.Children];
-     * 
-     *      // return first child
-     *      return Roact.oneChild(self.props[Roact.Children]);
-     * 
-     *      // iterate children
-     *      let children = self.props[Roact.Children];
-     *      if (children && children.length > 0)
-     *      {
-     *          for (let child of children)
-     *          {
-     *              // do something with child...
-     *          }
-     *      }
-     * 
-     *      // put each child in a frame
-     *      let children = self.props[Roact.Children]
-     *      children.map(child => Roact.createElement("Frame", {}, [child]))
-     */
-    const Children: 0xBAD_F00D_DEAD_0_0;
-
-    /**
-     * Use `Roact.Ref` as a key into the props of a primitive element to receive a handle to the underlying Roblox Instance.
-     * 
-     *      interface IExampleComponent { ref: Roact.Ref }
-     *      let exampleComponent = Roact.Component.extend<{}, {}, IExampleComponent>("ExampleComponent");
-     *      
-     *      exampleComponent.init = (self) => {
-     *          self.ref = Roact.createRef();
-     *      }
-     * 
-     *      exampleComponent.render = (self) => {
-     *          return Roact.createElement("Frame", {
-     *              [Roact.Ref]: self.ref
-     *          })
-     *      }
-     * 
-     *      exampleComponent.didMount = (self) => print("Roblox Instance", self.ref.current)
-     */
-    const Ref: "Roact.Ref";
-
-    const Portal: "Roact.Portal";
-
-    /**
-     * `Roact.None` is a special value that can be used to clear elements from your component 
-     * state when calling setState or returning from `getDerivedStateFromProps`.
-
-     * In Lua tables, removing a field from state is not possible by setting its value to `nil` 
-     *  because `nil` values mean the same thing as no value at all. If a field needs to be removed from state, 
-     *   it can be set to `Roact.None` when calling setState, which will ensure that the resulting state no longer 
-     *   contains it:
-     */
-    const None: undefined;
-
-    /** 
-     * Index into `Roact.Event` to receive a key that can be used to connect to events when creating primitive elements
-     */
-    const Event: {
-        [name: string]: "Event"
-    };
-
-    /** 
-     * Index into `Roact.Change` to receive a key that can be used to connect to `GetPropertyChangedSignal` events.
-     */
-    const Change: {
-        [name: string]: "GetPropertyChangedSignal"
-    };
-
     /**
      * Given a dictionary of children, returns a single child element.
      * 
@@ -150,202 +96,16 @@ declare namespace Roact {
      */
     function createRef<T extends Instance>(): Ref<T>;
 
-
-    class PureExtendedComponent<S ={}, P={}, V={}> extends ExtendedComponent<Readonly<S>, Readonly<P>, V>{
-
-    }
-
     type ContainsKeys<S, K extends keyof S> = (Pick<S, K> | S | null);
-    type ComponentRef<S, P, V> = ExtendedComponent<S, P, V> & V;
-
-    namespace self {
-        /**
-         * @internal
-         */
-        interface __base {
-            /**
-             * `getElementTraceback` gets the stack trace that the component was created in. 
-             * This allows you to report error messages accurately.
-             */
-            getElementTraceback(): string | undefined;
-        }
-
-
-
-        interface __pure<S, P> extends __base {
-            state: Readonly<S>;
-            props: P;
-        }
-
-        interface __update<S, P> extends __pure<S, P> {
-            /**
-             * `setState` requests an update to the component's state. Roact may schedule this update for a later time or resolve it immediately
-             * 
-             *      component.willUpdate = self => {
-             *          self.setState({name: 'value'})
-             *      }
-             * 
-             * 
-             *      component.willUpdate = self => {
-             *          self.setState((state, props) => {
-             *              return {name: state.name + 1};
-             *          })
-             *      }
-             * 
-             * @param state The current state
-             * @returns Any changed state properties
-             */
-            setState<K extends keyof S>(
-                state: (
-                    (
-                        prevState: Readonly<S>,
-                        props: P
-                    )
-                        => ContainsKeys<S, K> | ContainsKeys<S, K>)
-            ): void;
-        }
-
-        interface init<S = dynamic, P = dynamic> extends __update<S, P>, __base {
-            state: S;
-        }
-
-        interface render<S = dynamic, P = dynamic> extends __pure<S, P> { }
-        interface willUnmount<S = dynamic, P = dynamic> extends __pure<S, P> { }
-        interface willUpdate<S = dynamic, P = dynamic> extends __update<S, P> { }
-        interface didMount<S = dynamic, P = dynamic> extends __update<S, P> { }
-        interface didUpdate<S = dynamic, P = dynamic> extends __pure<S, P> { }
-        interface shouldUpdate<S = dynamic, P = dynamic> extends __pure<S, P> { }
-    }
-
-    /**
-     * Created through Roact.Component.extend(name)
-     */
-    class ExtendedComponent<S = {}, P = {}, V = {}> implements IExtendedComponent {
-        /**
-         * If `defaultProps` is defined on a stateful component, any props that aren't specified when a component 
-         * is created will be taken from there.
-         */
-        defaultProps?: P;
-
-        /**
-         * `init` is called exactly once when a new instance of a component is created. It can be used to set up the initial state, as well as any non-render related values directly on the component.
-         *
-         * Use `state` inside of init to set up your initial component state:
-         * 
-                myComponent.init = (self) => {
-                    self.state = {
-                        position = 0,
-                        velocity = 10
-                    };
-                }
-         */
-        init: (self: self.init<S, P> & V, props: P) => void;
-
-        /**
-         * `render` describes what a component should display at the current instant in time.
-         * 
-         * render must be defined for all components. The default implementation of render throws an error; 
-         * if your component does not render anything, define a render function that returns nil explicitly. 
-         * This helps make sure that you don't forget to define render!
-         */
-        render: (self: self.render<S, P> & V) => Roact.Element;
-
-        /**
-        * `willUnmount` is fired right before Roact begins unmounting a component instance's children.
-        * 
-        * `willUnmount` acts like a component's destructor, and is a good place to disconnect any manually-connected events.
-        */
-        willUnmount: (self: self.willUnmount<S, P> & V) => void;
-
-        /**
-         * `didMount` is fired after the component finishes its initial render. At this point, all associated Roblox Instances have been created, and all components have finished mounting.
-         * 
-         * `didMount` is a good place to start initial network communications, attach events to services, or modify the Roblox Instance hierarchy.
-         */
-        didMount: (self: self.didMount<S, P> & V) => void;
-
-        /**
-         * `didUpdate` is fired after at the end of an update. At this point, the reconciler has updated the properties of any Roblox Instances and the component instance's props and state are up to date.
-
-         * `didUpdate` is a good place to send network requests or dispatch Rodux actions, but make sure to compare `self.props` and `self.state` with `previousProps` and `previousState` to avoid triggering too many updates.
-         */
-        didUpdate: (self: self.didUpdate<S, P> & V, previousProps: Readonly<P>, previousState: Readonly<S>) => void;
-
-
-        /**
-         * `willUpdate` is fired after an update is started but before a component's state and props are updated.
-         *
-         * `willUpdate` can be used to make tweaks to your component's state using setState. Often, this should be done in `getDerivedStateFromProps` instead.
-         */
-        willUpdate: (self: self.willUpdate<S, P> & V, nextProps: P, nextState: S) => void;
-
-        /**
-         * `shouldUpdate` provides a way to override Roact's rerendering heuristics.
-            By default, components are re-rendered any time a parent component updates, or when state is updated via `setState`.
-            `PureComponent` implements `shouldUpdate` to only trigger a re-render any time the props are different based on shallow equality.
-         */
-        shouldUpdate: (self: self.shouldUpdate<S, P> & V, nextProps: P, nextState: S) => boolean;
-
-        /**
-         * Used to recalculate any state that depends on being synchronized with props.
-         * 
-         * Generally, you should use `didUpdate` to respond to props changing. If you find yourself copying props values to state as-is, consider using props or memoization instead.
-         * 
-         * `getDerivedStateFromProps` should return a table that contains the part of the state that should be updated.
-         */
-        getDerivedStateFromProps: <K extends keyof S>(nextProps: P, nextState: S) => ContainsKeys<S, K>;
-    }
 
     abstract class PureComponent<S = {}, P = {}> extends Component<S, P> {
-        /**
-         * Create a new stateful component with the specified name
-         * 
-         * Basic Construction:
-         * 
-         *      const Component = Roact.PureComponent.extend("Component");
-         * 
-         * Type-safe Construction:
-         * 
-         *      interface IComponentState { ... } // self.state.*
-         *      interface IComponentProps { ... } // self.props.*
-         *      interface IComponent { ... } // self.*
-         *      const Component = Roact.PureComponent.extend<IComponentState, IComponentProps, IComponent>("Component");
-         * 
-         * @param name The class name of this component
-         */
-        extend<state = dynamic, props = dynamic, fields = dynamic>(
-            name: string
-        ): PureExtendedComponent<state, props & IBaseProps, fields> & fields;
+
     }
 
-    /**
-     * A Roact Component, created using 
-     * @code Roact.Component.extend<...>(name)
-     */
     abstract class Component<S = {}, P = {}> extends IComponent {
         constructor(p: P);
 
-        /**
-         * Create a new stateful component with the specified name
-         * 
-         * Basic Construction:
-         * 
-         *      const Component = Roact.Component.extend("Component");
-         * 
-         * Type-safe Construction:
-         * 
-         *      interface IComponentState { ... } // self.state.*
-         *      interface IComponentProps { ... } // self.props.*
-         *      interface IComponent { ... } // self.*
-         *      const Component = Roact.Component.extend<IComponentState, IComponentProps, IComponent>("Component");
-         * 
-         * @param name The class name of this component
-         */
-        static extend<state = dynamic, props = dynamic, fields = dynamic>(
-            name: string
-        ): ExtendedComponent<state, props & IBaseProps, fields> & fields;
-
-        protected props: P & { children: Element[] };
+        public props: P & { children: Element[] };
         protected state: S;
 
         /**
@@ -385,13 +145,6 @@ declare namespace Roact {
     interface Ref<T extends Instance = Instance> {
         readonly current: T | undefined
     }
-
-    interface IBaseProps {
-        [0xBAD_F00D_DEAD_0_0]: Roact.Element[]
-    }
-
-    type dynamic = { [name: string]: any }
-
 
 	type JsxIntrinsic<T extends Rbx_Instance> = Partial<SubType<T, PropertyTypes>> & Rbx_JsxProps<T>;
 	type JsxIntrinsicContainer<T extends Rbx_Instance> = Rbx_JsxProps<T>;
