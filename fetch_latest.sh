61f0cdae234cd06ca569a547b284d8963e2a0c33
osyrisrblx <osyrisrblx@gmail.com>
2022-03-31 20:22:12 -0700

    Upgrade to Roact 1.4.2

---
+++ fetch_latest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+VERSION=1.4.2
+
+wget https://github.com/Roblox/roact/archive/refs/tags/v$VERSION.zip
+unzip -o v$VERSION.zip
+cp -r ./roact-$VERSION/src .
+rm -rf ./src/Component.spec
+rm -rf ./src/**/*.spec.lua
+rm -rf ./src/*.spec.lua
+rm -rf ./v$VERSION.zip
+rm -rf ./roact-$VERSION
